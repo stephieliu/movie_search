38644016c4fe64007e5415c361ba1fe032070580
stephie-liu <66147533+stephieliu@users.noreply.github.com>
2024-10-10 10:55:01 -0400

    Display a single static movie title on the webpage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import SearchIcon from './search.svg';
 // api key containing movie information
 const API_URL = 'http://www.omdbapi.com?apikey=ebe37347';
 
+const movie1 = {
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+    "Title": "Italian Spiderman",
+    "Type": "movie",
+    "Year": "2007",
+    "imdbID": "tt2705436"
+}
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -32,6 +39,31 @@ const App = () => {
                     // onChange allows you to actually modify the value
                     onChange={() => {}}
                 />
+                <img
+                    src = {SearchIcon}
+                    alt = "search icon"
+                    onClick={() =>{}}
+                />
+            </div>
+
+            <div className="container">
+            <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+
+                <div>
+                    <img
+                        src={movie1.Poster !== 'N/A'? movie1.Poster: 'https://placeholder.com/400'}
+                        alt={movie1.Title}
+                    />
+                </div>
+
+                <div>
+                    <span>{movie1.Type}</span>
+                    <h3>{movie1.Title}</h3>
+                </div>
+            </div>
             </div>
         </div>
     );
