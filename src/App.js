42b124b5f2e5ef66d0b1b9b6860ea7cc54e48048
stephie-liu <66147533+stephieliu@users.noreply.github.com>
2024-10-10 10:43:38 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useEffect} from 'react';
+import './App.css'; // Applies the css style to the webpage
+import SearchIcon from './search.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// api key containing movie information
+const API_URL = 'http://www.omdbapi.com?apikey=ebe37347';
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        // Call the api:
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        // Can check data using console (inspect webpage)
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+    return(
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="search for movies"
+                    // Can statically set a value (can no longer search anything)
+                    value="Superman"
+                    // onChange allows you to actually modify the value
+                    onChange={() => {}}
+                />
+            </div>
+        </div>
+    );
 }
 
+// Remember that components must be exported so they can be called
 export default App;
